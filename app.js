46e4899525bdf030780eb5d844b0b304e1bbf5f6
Zack Klinger <zklinger@gmail.com>
2016-10-02 14:45:44 -0700

    CREATE divs using css sprites for flag nodes and adjust links

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function buildForceLayout() {
       var width, height;
       var chartWidth, chartHeight;
       var margin;
+      var forceLayout = d3.select('.force-layout-graph');
       var svg = d3.select(".graph");
       var chartLayer = svg.append("g").classed("chartLayer", true);
 
@@ -29,18 +30,15 @@ function buildForceLayout() {
           .attr("width", chartWidth)
           .attr("height", chartHeight)
           .attr("transform", "translate(" + [margin.left, margin.top] + ")");
-
       }
 
       function drawChart(data) {
         var simulation = d3.forceSimulation()
           .force("link", d3.forceLink().id(function (d) {
             return d.index
-          }))
-          .force("collide", d3.forceCollide(function (d) {
-            return d.r + 8
-          }).iterations(16))
-          .force("charge", d3.forceManyBody())
+          }).distance(40).strength(.2))
+          .force("collide", d3.forceCollide(20).iterations(6))
+          .force("charge", d3.forceManyBody().theta(.5))
           .force("center", d3.forceCenter(chartWidth / 2, chartWidth / 2))
           .force("y", d3.forceY(0))
           .force("x", d3.forceX(0));
@@ -53,18 +51,12 @@ function buildForceLayout() {
           .append("line")
           .attr("stroke", "black");
 
-        var node = chartLayer.append("g")
-          .attr("class", "nodes")
-          .selectAll("circle")
+        var node = forceLayout.selectAll('.node')
           .data(data.nodes)
-          .enter().append("circle")
-          .attr("r", 5)
-          .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+          .enter().append('div')
+          .attr('class', function(d) { return 'flag flag-' + d.code; });
 
-        var ticked = function () {
+        var tick = function () {
           link
             .attr("x1", function (d) {
               return d.source.x;
@@ -80,23 +72,27 @@ function buildForceLayout() {
             });
 
           node
-            .attr("cx", function (d) {
-              return d.x;
+            .style("left", function (d) {
+              return (d.x - 16 + ((window.innerWidth - width) / 2)) + 'px';
             })
-            .attr("cy", function (d) {
-              return d.y;
-            });
+            .style("top", function(d) {
+              return (d.y - 16) + 'px';
+            })
+            .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended));
         };
 
         simulation
           .nodes(data.nodes)
-          .on("tick", ticked);
+          .on("tick", tick);
 
         simulation.force("link")
           .links(data.links);
 
         function dragstarted(d) {
-          if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+          if (!d3.event.active) simulation.alphaTarget(0.5).restart();
           d.fx = d.x;
           d.fy = d.y;
         }
@@ -115,7 +111,6 @@ function buildForceLayout() {
 
       setSize(data);
       drawChart(data);
-
     });
 }
 
@@ -136,7 +131,7 @@ class Chart extends Component {
   render() {
     return (
       <div className="force-layout-graph">
-        <svg className="graph" width={640} height={640}></svg>
+        <svg className="graph" width={768} height={768}></svg>
       </div>
     );
   }
